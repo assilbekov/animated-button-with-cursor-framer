7855b3886f5f022f097534646cc8b9cb49f388f5
assilbekov <askhat.assilbekov@gmail.com>
2024-04-07 10:45:20 -0300

    add basic stylign

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="w-full h-screen bg-slate-50 flex justify-center items-center relative">
+      <button className="py-4 px-32 bg-rose-700 border-1 border-rose-950 rounded-md text-slate-50 text-2xl">
+        Click me!
+      </button>
+      <img
+        src="/pointinghand.svg"
+        alt="Pointing hand cursor"
+        className="w-20 h-20 absolute"
+      />
+    </main>
+  );
 }
 
 export default App;
